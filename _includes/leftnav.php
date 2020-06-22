51d3d98278dbe03d5bdc7e32d2da345255665d15
DarlanSantos444 <darlan.santos.10420@gmail.com>
2020-06-22 17:43:27 -0300

    q10

--- _includes/leftnav.php
+++ _includes/leftnav.php
@@ -23,7 +23,7 @@
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
+               with font-awesome or any other icon font library  -->
           <li class="nav-item">
             <a href="index_dist.php" class="nav-link active">
               <i class="nav-icon fab fa-accessible-icon"></i>
