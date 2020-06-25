bce5540acfce8f4f44c75e76415bc965a66cd365
DarlanSantos444 <62038053+DarlanSantos444@users.noreply.github.com>
2020-06-25 19:07:16 -0300

    Delete funcao_desconto.php

--- funcao_desconto.php
+++
@@ -1,9 +0,0 @@
-<?php
-    //Função para calcular desconto na loja virtual
-    function calcular_desconto($valor_produto,$desconto){
-        $valor_desconto = $valor_produto * ($desconto/100);    
-        $valor_final = $valor_produto - $valor_desconto;
-        return $valor_final;
-    }
-
-?>
