0cb6f7ed4cfbffa737a1521cc85d1d0bbde19a4f
DarlanSantos444 <darlan.santos.10420@gmail.com>
2020-06-25 17:50:50 -0300

    q5

--- q5.php
+++ q5.php
@@ -8,102 +8,40 @@
             <h3 class="page-title">
             <span class="page-title-icon bg-gradient-primary text-white mr-2">
             <i class="mdi mdi-library-books"></i>
-            </span> Questão 5 </h3>
-            <h3><br>5. Um funcionário recebe aumento salarial anualmente. Sabe-se que: 
-• Esse funcionário foi contratado em 2015, com salário inicial de R$1000,00; 
-• Em 2016 recebeu aumento de 1,5% sobre seu salário inicial; 
-• A partir de 2017 (inclusive), os aumentos salariais sempre corresponderam ao dobro do percentual do ano anterior. Exiba o salário atual desse funcionário. </h3>
+            </span> Questão 5</h3>
+            <h3><br>5. Um funcionário recebe aumento salarial anualmente. Sabe-se que:<br> 
+            • Esse funcionário foi contratado em 2015, com salário inicial de R$1000,00; <br> 
+            • Em 2016 recebeu aumento de 1,5% sobre seu salário inicial; <br> 
+            • A partir de 2017 (inclusive), os aumentos salariais sempre corresponderam ao dobro do percentual do ano anterior. Exiba o salário atual desse funcionário. </h3>
         </div>
 
-        <?php
+<?php
     
-    $ano2015 = 1000;
+    $salario15 = 1000;
+    $salario16 = (1000+15);
+    $salario17 = (1015+30,45);
+    $salario18 = (1045,45+62,72);
+    $salario19 = (1108,17+132,98);
+    $salario20 = 1241,15;  
 
-    function pri_ano($ano2015){
-      $ano2015 =  $ano2015;
-      return $ano2015;
-    }
-
-    function ano2016($ano2015){
-      $A2016 = ($ano2015*0.015);
-      $resul2016 = ($ano2015+$A2016);
-      return $resul2016;
-    }
-
-    function ano2017($resul2016){
-      $perc  = (0.015*2);
-      $valor = ( ($perc*1015) + 1015);
-      return $valor;
-    }
-
-    function ano2018($ano2015){
-      $perc = (0.03*2);
-      $valor = ( ($perc*1045.45) + 1045.45);
-      return $valor;
-    }
-
-    function ano2019($ano2015){
-      $perc = (0.06*2);
-      $valor = ( ($perc*1108.17) + 1108.17);
-      return $valor;
-    }
-
-    function ano2020($ano2015){
-      $perc = (0.12*2);
-      $valor = ( ($perc*1241.15) + 1241.15);
-      return $valor;
-    }
-
-    
-    
 ?>
 
             <div class="row">
                 <div class="col-md-7 grid-margin stretch-card">
                     <div class="card">
                         <div class="card-body">
-                            <div class="clearfix">
-                                <h4 class="card-title float-left">Média <i class="mdi mdi-calculator"></i></h4>
-                                <div id="visit-sale-chart-legend" class="rounded-legend legend-horizontal legend-top-right float-right"></div>
-                            </div>
-                            <!--Conteudo do card-->
-                            
-                            <form class="forms-sample" id="formSoma" name="formSoma" action="" method="POST">
-                                <div class="form-group">
-                                    <label for="exampleInputUsername1">Número 1</label>
-                                    <input type="number" class="form-control" id="n1" name="n1" required>
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleInputEmail1">Número 2</label>
-                                    <input type="number" class="form-control" id="n2" name="n2" required>
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleInputEmail1">Número 3</label>
-                                    <input type="number" class="form-control" id="n3" name="n3" required>
-                                </div>
-                                
-                                <button type="submit" class="btn btn-info">Calcular</button>
-                            </form>
-
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-5 grid-margin stretch-card">
-                    <div class="card">
-                        <div class="card-body">
-                            <h4 class="card-title">Resultado<i class="mdi mdi-file-document-box"></i></h4>
+                        <h4 class="card-title">Resultado<i class="mdi mdi-file-document-box"></i></h4>
                             <div id="traffic-chart-legend" class="rounded-legend legend-vertical legend-bottom-left pt-4"></div>
-                            <p>Primeiro Número: <?php echo $n1;?></p>
-                            <p>Segundo Número: <?php echo $n2;?></p>
-                            <p>Terceiro Número: <?php echo $n3;?></p>
-                            <p>Média: <?php echo number_format($media, 2);?></p>
+                            <p>Salário 2015: <?php echo $salario15;?></p>
+                            <p>Salário 2016: <?php echo $salario16;?></p>
+                            <p>Salário 2017: <?php echo $salario17;?></p>
+                            <p>Salário 2018: <?php echo $salario18;?></p>
+                            <p>Salário 2019: <?php echo $salario19;?></p>
+                            <p>Salário 2020: <?php echo $salario20;?></p>
                         </div>
-                        <!--Conteudo do card-->
-
-
                     </div>
                 </div>
+
             </div>        
 
 <?php
